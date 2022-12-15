3675253b62bb1c4c35167e297f2157b5f9fd3963
leskevich <leskevichtema@gmail.com>
2022-12-15 15:32:35 +0100

    one part

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Skills.module.css'
 import sContainer from './../common/styles/Container.module.css'
+import {Skill} from "./skill/Skill";
 
 export const Skills = () => {
     return (
@@ -8,7 +9,10 @@ export const Skills = () => {
             <div className={`${sContainer.container} ${s.skillsContainer}`}>
                 <h2>Skills</h2>
                 <div className={s.skills}>
-
+                    <Skill title={'HTML/CSS'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                    <Skill title={'JS'} description={'Ad commodi consectetur'}/>
+                    <Skill title={'React'} description={'sunt tempore temporibus vel voluptas voluptate!'}/>
                 </div>
             </div>
         </div>
